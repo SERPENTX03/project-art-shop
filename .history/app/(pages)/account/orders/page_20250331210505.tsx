a13f9f7ab2564c8f1865d542cc123c57b1a3af56
SERPENTX03 <dashlove236@gmail.com>
2025-03-31 21:29:42 +0700

    OrderList Checkout success

--- .history/app/(pages)/account/orders/page_20250331210505.tsx
+++ .history/app/(pages)/account/orders/page_20250331210505.tsx
@@ -1,13 +1,9 @@
 import { fetchOrderById } from "@/actions/order";
-import OrderListClient from "@/components/orderuser/OrderListClient";
 import { auth } from "@clerk/nextjs/server";
 
 const OrderByAccout = async () => {
   const { userId } = await auth();
-  if (!userId) {
-    throw new Error("User ID is null. Unable to fetch order.");
-  }
   const orderUser = await fetchOrderById(userId);
-  return <OrderListClient orders={orderUser} />;
+  return <div>OrderByAccout</div>;
 };
 export default OrderByAccout;
---
+++ .history/components/orderuser/OrderListClient_20250331210736.tsx
@@ -0,0 +1,4 @@
+const OrderListClient = () => {
+  return <div>OrderListClient</div>;
+};
+export default OrderListClient;
